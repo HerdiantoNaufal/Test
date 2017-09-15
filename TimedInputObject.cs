4421e167c4137abe81ec204e3c08f1075f9a127b
Herdianto Naufal Farras <fherdiantonaufal@yahoo.com>
2017-09-15 10:55:09 +0700

    second
    second

---
+++ TimedInputObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedInputObject : MonoBehaviour, TimedInputHandler {
+    public Text data;
+    public GameObject name;
+    public string name2;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.white;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HandleTimedInput()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        name = GameObject.Find("sphere");
+        name2 = name.ToString();
+        StartCoroutine(barang());
+    }
+
+    IEnumerator barang()
+    {
+        WWW itemsdata = new WWW("http://somethingnotright.dx.am/barang.php");
+        yield return itemsdata;
+        WWW jumlah = new WWW("http://somethingnotright.dx.am/jumlah.php");
+        yield return jumlah;
+        string itemsdatastring = itemsdata.text;
+        print(itemsdatastring);
+        data.text = itemsdatastring;
+    }
+}
